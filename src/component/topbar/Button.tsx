c8a8eb913762af1b89bb6fd6581efedf32c79056
Michael Mannseicher <m.mansi1@gmx.net>
2021-03-15 01:46:35 +0100

    add: style to head

--- src/component/topbar/Button.tsx
+++ src/component/topbar/Button.tsx
@@ -1,5 +1,6 @@
 import {IVirtualNode, tsx} from "springtype";
 import {jss} from "st-jss";
+import {makeStyles} from "../../function/makeStyles";
 
 export interface TopBarButtonProps {
     startIcon?: IVirtualNode;
@@ -8,24 +9,23 @@ export interface TopBarButtonProps {
     children?: IVirtualNode;
     href?: string;
 }
+const classes = makeStyles({
+    root: {
+        display: 'flex',
+        paddingLeft: 15,
+        paddingRight: 15,
+        fontSize: 18,
+        lineHeight: '60px',
+        paddingTop: 5,
+        textDecoration: 'none',
+        color: '#fff',
+        paddingBottom: 5,
+        cursor: 'pointer'
+    }
+});
 
 export const Button = ({onClick, children, startIcon, endIcon}: TopBarButtonProps) => {
-    const [classes, style] = jss.makeStyles({
-        root: {
-            display: 'flex',
-            paddingLeft: 15,
-            paddingRight: 15,
-            fontSize: 18,
-            lineHeight: '60px',
-            paddingTop: 5,
-            textDecoration: 'none',
-            color: '#fff',
-            paddingBottom: 5,
-            cursor: 'pointer'
-        }
-    });
     return <a onClick={onClick} class={classes.root}>
-        {style}
         {startIcon}
         {children}
         {endIcon}
